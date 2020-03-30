38d15f944b1953a20c82abe0181380e30fadcc1b
dakaras <dan.karagiannis@protonmail.ch>
2020-03-30 10:08:21 -0500

    added case statement received message

--- src/Store.js
+++ src/Store.js
@@ -2,11 +2,33 @@ import React from 'react'
 
 const CTX = React.createContext()
 
+/*
+    msg {
+        from: 'user',
+        msg: 'hi',
+        topic: 'general
+    }
+    
+    state {
+        general: [{msg1}, {msg2}, {msg3}, {newMsg}],
+        topic2: []
+    }
+
+*/
+
 function reducer(state, action){
+    const [from, msg, topic] = action.payload
     switch(action.type){
         case 'RECEIVE_MESSAGE': 
         return {
-
+            ...state,
+            [topic]: [
+                ...state[topic],
+                {
+                    from: from,
+                    msg: msg
+                }
+            ]
         }
         default: return state 
     }
