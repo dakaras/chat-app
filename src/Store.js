ba6db28ff750ef2280442a0596562bb828d6bef4
dakaras <dan.karagiannis@protonmail.ch>
2020-03-30 09:59:09 -0500

    added store.js

---
+++ src/Store.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const CTX = React.createContext()
+
+function reducer(state, action){
+    switch(action.type){
+        case 'RECEIVE_MESSAGE': 
+        return {
+
+        }
+        default: return state 
+    }
+}
+
+export default function Store(props){
+
+    const reducerHook = React.useReducer(reducer, initialState)
+    return (
+        <CTX.Provider value={}>
+            {props.children}
+        </CTX.Provider>
+    )
+}
